30f8920b5f8f7b67c6a59e3ad46790eaac0e962d
DrAmonov <temurxonamonov@gmail.com>
2022-12-06 19:46:57 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,50 @@
+// // --------------------------1-exercise------------------------
+// function addition(a, b) {
+// 	return(a+b);
+// 	console.log(addition);
+// }
+// // --------------------------2-exercise------------------------
+// function convert(minutes) {
+// 	return(minutes*60);
+// 	console.log(minutes);
+// }
+// // --------------------------3-exercise------------------------
+// function addition(num) {
+// 	return(num+1);
+// 	console.log(num);
+// }
+// // --------------------------4-exercise------------------------
+// function triArea(base, height) {
+// 	return((base * height) / 2);
+// 	console.log(triArea);
+// }
+// // --------------------------5-exercise------------------------
+// function calcAge(age) {
+// 	return(age*365);
+// 	console.log(age);
+// }
+// // --------------------------6-exercise------------------------
+// function remainder(x, y) {
+// 	return(x % y);
+// 	console.log(remainder);
+// }
+// // --------------------------7-exercise------------------------
+// function footballPoints(wins, draws, losses) {
+// 	return((wins*3) + (draws*1) + (losses*0));
+// 	console.log(footballPoints);
+// }
+// // --------------------------8-exercise------------------------
+// function points(twoPointers, threePointers) {
+// 	return((twoPointers*2)+(threePointers*3));
+// 	console.log(points);
+// }
+// // --------------------------9-exercise------------------------
+// function lessThan100(a, b) {
+// 	return a + b < 100 ? true :false;
+// 	console.log(lessThan100);
+// };
+// // --------------------------10-exercise------------------------
+// function animals(chickens, cows, pigs) {
+// 	return((chickens*2)+(cows*4)+(pigs*4));
+// 	console.log(animals);
+// }
